fdbb381e85d3ec842de8b28980803f26d27c1c43
vboxuser <vboxuser@Ubuntu22.myguest.virtualbox.org>
2023-09-06 20:33:14 -0300

    First Commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">Nome</label>
+        <div class="col-sm-10">
+        <input type="name" class="form-control" id="inputName" placeholder="Nome">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@exemplo.com">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Senha</label>
+        <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword" placeholder="Senha">
+        </div>
+    </div>
+    </form>
+</body>
+</html>
